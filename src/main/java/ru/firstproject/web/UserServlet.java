b94a2d9151dbca9b4af9ff79c55297fdebaddc08
Your Name <your.mail@host.com>
2018-11-09 18:31:17 +0300

    logback logging

--- src/main/java/ru/firstproject/web/UserServlet.java
+++ src/main/java/ru/firstproject/web/UserServlet.java
@@ -1,13 +1,19 @@
 package ru.firstproject.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class UserServlet extends javax.servlet.http.HttpServlet {
+    public static Logger logger = LoggerFactory.getLogger(UserServlet.class);
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println(System.getProperty("user.dir"));
+        logger.info("UserServlet doGet method");
         request.getRequestDispatcher("/users.jsp").forward(request, response);
     }
 }
