fc643896702e0ce85ac784a9b83db3cf131bab2b
Your Name <your.mail@host.com>
2018-11-09 11:02:33 +0300

    firstDeploy

---
+++ src/main/java/ru/firstproject/web/UserServlet.java
@@ -0,0 +1,13 @@
+package ru.firstproject.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class UserServlet extends javax.servlet.http.HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/users.jsp").forward(request, response);
+    }
+}
